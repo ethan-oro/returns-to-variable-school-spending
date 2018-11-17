82484fbe0f1c69550087d8e2a0e1f9291ec84eaf
Isaac Kasevich <isaackasevich@Isaacs-MacBook-Pro-5.local>
2018-11-17 13:53:05 -0800

    Read csv into pd

---
+++ src/imports.py
@@ -0,0 +1,15 @@
+# sys imports
+import sys, os, shutil, errno
+
+# string/data inputs
+import string, csv, json, fileinput
+
+# gucci imports
+import numpy as np
+import pandas as pd
+import sklearn as skl
+
+# misc
+import progressbar
+import collections
+import pickle #for saving data objects
