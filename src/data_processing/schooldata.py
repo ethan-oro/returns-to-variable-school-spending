161d67ed3b009cf35cf6e9b0bf91b336db03b648
Isaac Kasevich <isaackasevich@Isaacs-MacBook-Pro-5.local>
2018-11-17 15:00:09 -0800

    categories

--- src/data_processing/schooldata.py
+++ src/data_processing/schooldata.py
@@ -23,15 +23,80 @@ def main():
 		
 def load_csv(filename):
 	df = pd.read_csv(filename, sep =',')
-	print (df['Grade'])
+	input_cols = list(df.columns)[:62]
+	process(df)
 	return df 
 
 def process(frame):
 	'''
-	
+	A. Split into elementary, middle, high schools
+
 	'''
+	categories = {
+		'descriptive': ['School Code', 
+						'School Name', 
+						'Town', 
+						'State', 
+						'Zip', 
+						'District Name',
+						'District Code'],
+		'endogeneous_input': [
+			'School Type',
+			'PK_Enrollment',
+			'K_Enrollment',
+			'1_Enrollment',
+			'2_Enrollment',
+			'3_Enrollment',
+			'4_Enrollment',
+			'5_Enrollment',
+			'6_Enrollment',
+			'7_Enrollment',
+			'8_Enrollment',
+			'9_Enrollment',
+			'10_Enrollment',
+			'11_Enrollment',
+			'12_Enrollment',
+			'SP_Enrollment',
+			'TOTAL_Enrollment',
+			'First Language Not English',
+			'%% First Language Not English',
+			'English Language Learner',
+			'%% English Language Learner',
+			'Students With Disabilities',
+			'%% Students With Disabilities',
+			'High Needs',
+			'%% High Needs',
+			'Economically Disadvantaged',
+			'%% Economically Disadvantaged',
+			'%% African American',
+			'%% Asian',
+			'%% Hispanic',
+			'%% White',
+			'%% Native American',
+			'%% Native Hawaiian, Pacific Islander',
+			'%% Multi-Race, Non-Hispanic',
+			'%% Males',
+			'%% Females',
+			'Number of Students'
+		],
+		'exogeneous_input': [
+			'Total # of Classes',
+			'Average Class Size',
+			'Salary Totals',
+			'Average Salary',
+			'FTE Count',
+			'In-District Expenditures',
+			'Total In-district FTEs',
+			'Average In-District Expenditures per Pupil',
+			'Total Expenditures',
+			'Total Pupil FTEs',
+			'Average Expenditures per Pupil'
+		],
+		'output_markers': []
+	}
 
-
+	feature_categories = categories['endogeneous_input'] + categories['exogeneous_input']
+	print (',\n'.join(feature_categories))
 
 
 
